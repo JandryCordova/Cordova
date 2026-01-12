545caf25b8bd21d147a67377fbf1f8c8e9f85c67
JandryCordova <jandry.torres.03@gmail.com>
2026-01-11 23:25:43 -0500

    el boton modificar imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("Imagen1");
+    cmpImagenSaludo.src="./imagenes/GANE2.gif"
     console.log("Ingresar a saludar")
 }
