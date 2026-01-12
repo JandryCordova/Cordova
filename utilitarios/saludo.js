eaf89cad7a6fb1000f4da904ddcbd63931ba8c5f
JandryCordova <jandry.torres.03@gmail.com>
2026-01-12 12:17:53 -0500

    Se implemento las funcinones recuperarInt y recuperarFloat

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,6 +1,13 @@
 saludar=function(){
     let nombre=recuperrarTexto("txtNombre");
     let Apellido=recuperrarTexto("txtApellido");
+    let Edad=recuperrarInt("txtEdad")
+    let Estatura=recuperrarFloat("txtEstatura")
+    console.log(nombre);
+    console.log(Apellido);
+    console.log(Edad);
+    console.log(Estatura);
+
 }
 
 recuperrarTexto=function(idcomponente){
@@ -8,4 +15,16 @@ recuperrarTexto=function(idcomponente){
     let valorRecuperado=nombre.value;
     return valorRecuperado;
 
+}
+
+recuperrarInt=function(idcomponente){
+    let valorCaja=recuperrarTexto(idcomponente);
+    let Entero=parseInt(valorCaja);
+    return Entero;
+}
+
+recuperrarFloat=function(idcomponente){
+    let valorCaja=recuperrarTexto(idcomponente);
+    let Decimal=parseFloat(valorCaja);
+    return Decimal.toFixed(2);
 }
