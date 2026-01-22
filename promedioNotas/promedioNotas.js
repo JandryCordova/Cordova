3a25c03b9d11e40fb9075a03bc92ae8216e43be9
JandryCordova <jandry.torres.03@gmail.com>
2026-01-21 23:36:06 -0500

    Archivo promedio de notas sin modificar nada

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,13 @@
+calcularPromedioNotas=function(){
+    let nota1=recuperarFlotante("txtNota1")
+    let nota2=recuperarFlotante("txtNota2")
+    let nota3=recuperarFlotante("txtNota3")
+    let promedio=calcularPromedio(nota1,nota2,nota3)
+    cambiarTexto("lblResultado",promedio.toFixed(2))
+    if (promedio>7){
+        cambiarImagen("imgResultado","./imagenes/exito.gif");
+    }else{
+        cambiarImagen("imgResultado","./imagenes/fracaso.gif");
+    }
+    
+}
