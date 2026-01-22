6c6a8f72e1b05425e20efbe06d31ee435a471c24
JandryCordova <jandry.torres.03@gmail.com>
2026-01-21 23:57:46 -0500

    Implementacion de Imagenes y textos de Reprobado, Buen Trabajo, Excelente, Datos Incorrectos

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -4,10 +4,17 @@ calcularPromedioNotas=function(){
     let nota3=recuperarFlotante("txtNota3")
     let promedio=calcularPromedio(nota1,nota2,nota3)
     cambiarTexto("lblResultado",promedio.toFixed(2))
-    if (promedio>7){
-        cambiarImagen("imgResultado","./imagenes/exito.gif");
+    if (promedio<5 && promedio>=0){
+        cambiarTexto("lblRespuesta","Reprobado")
+        cambiarImagen("imgResultado","./imagenes/reprobado.gif");
+    }else if (promedio>=5 && promedio<=8){
+        cambiarTexto("lblRespuesta","Buen Trabajo")
+        cambiarImagen("imgResultado","./imagenes/buentrabajo.gif");
+    }else if (promedio>8 && promedio<=10){
+        cambiarTexto("lblRespuesta","Excelente")
+        cambiarImagen("imgResultado","./imagenes/excelente.gif");
     }else{
-        cambiarImagen("imgResultado","./imagenes/fracaso.gif");
+        cambiarTexto("lblRespuesta","Datos Incorrectos")
+        cambiarImagen("imgResultado","./imagenes/datosIncorrecto.gif");
     }
-    
 }
