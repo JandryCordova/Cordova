8c47f5a36677e6cb03c69cfd8d1c42d62c441e63
JandryCordova <jandry.torres.03@gmail.com>
2026-01-27 16:04:23 -0500

    generarElemento

--- juego/serviciosJuego.js
+++ juego/serviciosJuego.js
@@ -2,5 +2,19 @@ obtenerAleatorio=function(){
     let aleatorio=Math.random(3);
     let Numero = (aleatorio * 3) + 1;
     let entero = parseInt(Numero);
-    console.log(entero);
+    console.log(entero)
+    return entero;
+}
+
+generarElemento=function(){
+    let resultado=obtenerAleatorio();
+    let respuesta = "";
+    if (resultado == 1){
+        respuesta = console.log("PIEDRA");
+    }else if(resultado == 2){
+        respuesta = console.log("PAPEL");
+    }else if(resultado == 3){
+        respuesta = console.log("TIJERA");
+    }
+    return respuesta;
 }
