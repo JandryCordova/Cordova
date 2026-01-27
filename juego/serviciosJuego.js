3fd2a9c7105f7eae7f692d97475eec6e95ef5957
JandryCordova <jandry.torres.03@gmail.com>
2026-01-27 17:00:09 -0500

    funcion determinarGanador, lista

--- juego/serviciosJuego.js
+++ juego/serviciosJuego.js
@@ -10,11 +10,29 @@ generarElemento=function(){
     let resultado=obtenerAleatorio();
     let respuesta = "";
     if (resultado == 1){
-        respuesta = console.log("PIEDRA");
+        respuesta = "PIEDRA";
     }else if(resultado == 2){
-        respuesta = console.log("PAPEL");
+        respuesta = "PAPEL";
     }else if(resultado == 3){
-        respuesta = console.log("TIJERA");
+        respuesta = "TIJERA";
     }
     return respuesta;
+}
+
+determinarGanador=function(eleccionJugador1,eleccionJugador2){
+    if ( eleccionJugador1 == eleccionJugador2){
+        return 0;
+    }
+    if (eleccionJugador1 == "PAPEL" && eleccionJugador2 == "PIEDRA"){
+        console.log(eleccionJugador1)
+        return 1;
+    }
+    if (eleccionJugador1 == "PIEDRA" && eleccionJugador2 == "TIJERA"){
+        return 1;
+    }
+    if (eleccionJugador1 == "TIJERA" && eleccionJugador2 == "PAPEL"){
+        return 1;
+    }
+    console.log(eleccionJugador2)
+    return 2;
 }
