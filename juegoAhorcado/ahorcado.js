f150e7057092b5ff698f3676bc7bb593697a7add
JandryCordova <jandry.torres.03@gmail.com>
2026-01-28 14:08:34 -0500

    Creamos la funcion guardarPalabra, y validamos que la palabra tenga 5 caracteres en mayuscula

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,1 +1,26 @@
-//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta = "";
+
+esMayuscula = function(caracter){
+    let Codigo =caracter.charCodeAt(0)
+    if (Codigo >= 65 && Codigo<=90){
+        return true;
+    }
+    return false;
+}
+
+guardarPalabra = function (){
+    let mayuscula = 0;
+    let palabra =recuperarTexto("txtSecreta");
+    if (palabra.length == 5){
+        for (a=0;a<palabra.length;a++){
+            let caracter = palabra.charAt(a)
+            if (esMayuscula(caracter)){
+                mayuscula++;
+            }
+        }
+        palabraSecreta = palabra;
+
+    }else{
+        console.log("Debe solo tener 5 caracteres")
+    }
+}
