84be322ed555636de66abe3ea9b0806a28998a4e
JandryCordova <jandry.torres.03@gmail.com>
2026-01-28 17:52:48 -0500

    funciones mostrar letra y validar, listas

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -12,15 +12,44 @@ guardarPalabra = function (){
     let mayuscula = 0;
     let palabra =recuperarTexto("txtSecreta");
     if (palabra.length == 5){
-        for (a=0;a<palabra.length;a++){
+        for ( let a=0;a<palabra.length;a++){
             let caracter = palabra.charAt(a)
             if (esMayuscula(caracter)){
                 mayuscula++;
             }
         }
         palabraSecreta = palabra;
+        console.log(palabraSecreta);
 
     }else{
-        console.log("Debe solo tener 5 caracteres")
+            alert ("Debe ingresar una palabra de 5 Letras Mayusculas");
+    }
+}
+
+mostrarLetra = function (letra, posicion){
+    if (posicion == 0){ 
+        document.getElementById("div0").textContent = letra; 
+    } 
+    if (posicion == 1){ 
+        document.getElementById("div1").textContent = letra; 
+    } 
+    if (posicion == 2){ 
+        document.getElementById("div2").textContent = letra; 
+    } 
+    if (posicion == 3){ 
+        document.getElementById("div3").textContent = letra; 
+    } 
+    if (posicion == 4){ 
+        document.getElementById("div4").textContent = letra; 
+    }
+}
+
+validar = function (letra){
+    let letrasEncontradas = 0;
+    for (let a=0 ; a<palabraSecreta.length;a++){
+        if ( a == letra){
+            mostrarLetra(letra,a);
+            letrasEncontradas++;
+        }
     }
 }
