a177e51c4ab966aa8c4b7c63ec50ead06f3e24fb
JandryCordova <jandry.torres.03@gmail.com>
2026-01-31 00:37:35 -0500

    funcion crearProducto

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -13,3 +13,27 @@ probarAtributos=function(){
         console.log("esta vivo");
     }
 }
+
+crearProducto=function(){
+    let producto1={
+        nombre : "Celular",
+        precio : 82.5,
+        stock : 100
+    }
+    let producto2={
+        nombre : "Reloj",
+        precio : 25.86,
+        stock : 200
+    }
+    console.log(producto1.nombre);
+    console.log(producto2.precio);
+    if(producto1.stock > producto2.stock){
+        console.log(producto1.nombre+" tiene mayor stock");
+    }
+    if(producto2.stock > producto1.stock ){
+        console.log(producto2.nombre+" tiene mayor stock");
+    }
+    if(producto1.stock == producto2.stock){
+        console.log(" Ambos productos tienen el mismo stock");
+    }
+}
