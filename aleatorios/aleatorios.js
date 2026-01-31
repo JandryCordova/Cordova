190e6d36ccb858df699d7611e6d8d9bef938f3f6
JandryCordova <jandry.torres.03@gmail.com>
2026-01-30 22:41:41 -0500

    Vaciamos el arreglo al generar aleatorio

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -11,27 +11,26 @@ numerosAleatorios = function(){
 
 generarAleatorios = function(){
     let numeroRecu = recuperrarInt("txtNumero");
+    aleatorios=[];
     if (numeroRecu>=5 && numeroRecu<=20){
         for ( let a=0 ; a<numeroRecu;a++){
             console.log("Indice",a);
             let ValorObt = numerosAleatorios();
-            aleatorios = ValorObt
+            aleatorios.push(ValorObt);
             console.log( aleatorios);
         }
         mostrarResultados(aleatorios);
     }else{
         console.log("Este numero no esta dentro del rango del 5 al 20")
     }
-        
-    
 }
 
 mostrarResultados = function(arregloNumeros){
     let cmpTabla = document.getElementById("ResultadoTabla");
     let contenidoTabla="<table>";
-    for (a=0 ; a<arregloNumeros; a++){
+    for (a=0 ; a<arregloNumeros.length; a++){
         contenidoTabla+="<tr><td>";
-        contenidoTabla+=arregloNumeros;
+        contenidoTabla+=arregloNumeros[a];
         contenidoTabla+="</tr></td>";
     }
     contenidoTabla+="</table";
